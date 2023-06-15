b5f538b027dd0dc024ed3bb258ce14b7586bc243
Tú Nguyễn <92352094+tubuzu@users.noreply.github.com>
2023-06-15 09:35:35 +0700

    complete

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public Movement movement { get; private set; }
+    public GhostHome home { get; private set; }
+    public GhostScatter scatter { get; private set; }
+    public GhostChase chase { get; private set; }
+    public GhostFrightened frightened { get; private set; }
+    public GhostBehavior initialBehavior;
+    public Transform target;
+    public int points = 200;
+
+    private void Awake()
+    {
+        this.movement = GetComponent<Movement>();
+        this.home = GetComponent<GhostHome>();
+        this.scatter = GetComponent<GhostScatter>();
+        this.chase = GetComponent<GhostChase>();
+        this.frightened = GetComponent<GhostFrightened>();
+    }
+
+    // private void Start()
+    // {
+    //     ResetState();
+    // }
+
+    public void ResetState()
+    {
+        this.gameObject.SetActive(true);
+        this.movement.ResetState();
+
+        this.frightened.Disable();
+        this.chase.Disable();
+        this.scatter.Disable();
+
+        if (this.home != this.initialBehavior)
+        {
+            this.home.Disable();
+        }
+
+        if (this.initialBehavior != null)
+        {
+            this.initialBehavior.Enable();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+        {
+            if (this.frightened.enabled)
+            {
+                FindObjectOfType<GameManager>().GhostEaten(this);
+            }
+            else
+            {
+                FindObjectOfType<GameManager>().PacmanEaten();
+            }
+        }
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        // Keep the z-position the same since it determines draw depth
+        position.z = transform.position.z;
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/GhostEyes.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostEyes : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Movement movement { get; private set; }
+
+    public Sprite up;
+    public Sprite down;
+    public Sprite left;
+    public Sprite right;
+
+    private void Awake()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.movement = transform.parent.GetComponent<Movement>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.movement.direction == Vector2.up) this.spriteRenderer.sprite = this.up;
+        if (this.movement.direction == Vector2.down) this.spriteRenderer.sprite = this.down;
+        if (this.movement.direction == Vector2.left) this.spriteRenderer.sprite = this.left;
+        if (this.movement.direction == Vector2.right) this.spriteRenderer.sprite = this.right;
+    }
+}
